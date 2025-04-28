1d425944a86931f0163c99c7d43bb4ec278355e2
YassYassId <idrissiy371@gmail.com>
2025-04-28 15:05:03 +0100

    First commit!

---
+++ src/main/java/com/jee/ebanking_backend/EBankingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jee.ebanking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBankingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jee/ebanking_backend/EBankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jee.ebanking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
